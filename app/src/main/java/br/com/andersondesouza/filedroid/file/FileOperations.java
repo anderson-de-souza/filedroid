d15e6833468774a82f156e8ee9fc7fb2931c7496
Anderson A. de Souza <anderson-de-souza@outlook.com>
2026-02-22 20:29:19 -0300

    feat update: file operations

--- app/src/main/java/br/com/andersondesouza/filedroid/file/FileOperations.java
+++ app/src/main/java/br/com/andersondesouza/filedroid/file/FileOperations.java
@@ -7,14 +7,14 @@ import java.io.IOException;
 
 public class FileOperations {
 
-    public static boolean renameFile(File origin, String newName) {
+    public static Results rename(File origin, String newName) {
 
         if (origin == null || newName == null) {
-            return false;
+            return Results.NULL_ARGUMENT;
         }
 
         if (!origin.exists()) {
-            return false;
+            return Results.NOT_FOUND;
         }
 
         File destination = null;
@@ -27,35 +27,43 @@ public class FileOperations {
         }
 
         if (destination.exists()) {
-            return false;
+            return Results.ALREADY_EXISTS;
         }
 
-        return origin.renameTo(destination);
+        if (origin.renameTo(destination)) {
+            return Results.OK;
+        }
+
+        return Results.IO_ERROR;
 
     }
 
-    public static boolean deleteRegularFile(File origin) {
+    public static Results deleteRegularFile(File origin) {
 
         if (origin == null) {
-            return false;
+            return Results.NULL_ARGUMENT;
         }
 
         if (!origin.isFile()) {
-            return  false;
+            return  Results.NOT_A_FILE;
         }
 
-        return origin.delete();
+        if (origin.delete()) {
+            return Results.OK;
+        }
+
+        return Results.IO_ERROR;
 
     }
 
-    public static boolean deleteDirectoryTree(File origin) {
+    public static Results deleteDirectoryTree(File origin) {
 
         if (origin == null) {
-            return false;
+            return Results.NULL_ARGUMENT;
         }
 
         if (!origin.isDirectory()) {
-            return  false;
+            return Results.NOT_A_DIRECTORY;
         }
 
         File[] children = origin.listFiles();
@@ -65,45 +73,57 @@ public class FileOperations {
             for (File child : children) {
 
                 if (child.isDirectory()) {
-                    if (!deleteDirectoryTree(child)) {
-                        return false;
+
+                    Results result = deleteDirectoryTree(child);
+
+                    if (result != Results.OK) {
+                        return result;
                     }
+
                 } else if (child.isFile()) {
-                    if (!deleteRegularFile(child)) {
-                        return false;
+
+                    Results result = deleteRegularFile(child);
+
+                    if (result != Results.OK) {
+                        return result;
                     }
+
                 }
 
             }
         }
 
-        return origin.delete();
+        if (origin.delete()) {
+            return Results.OK;
+        }
+
+        return Results.IO_ERROR;
 
     }
 
-    public static boolean copyFile(File origin, File destination, boolean overwrite) {
+    public static Results copyFile(File origin, File destination, boolean overwrite) {
 
         if (origin == null || destination == null) {
-            return false;
+            return Results.NULL_ARGUMENT;
         }
 
         if (!origin.isFile()) {
-            return false;
+            return Results.NOT_A_FILE;
         }
 
         if (destination.isDirectory()) {
-            return false;
+            return Results.NOT_A_FILE;
         }
 
         if (!overwrite && destination.exists()) {
-            return false;
+            return Results.ALREADY_EXISTS;
         }
 
         File parent = destination.getParentFile();
 
         if (parent != null && !parent.exists()) {
             if (!parent.mkdirs()) {
-                return false;
+                return Results.IO_ERROR;
             }
         }
 
@@ -118,30 +138,30 @@ public class FileOperations {
             }
 
         } catch (IOException e) {
-            return false;
+            return Results.IO_ERROR;
         }
 
-        return true;
+        return Results.OK;
 
     }
 
-    public static boolean copyDirectoryTree(File origin, File destination) {
+    public static Results copyDirectoryTree(File origin, File destination) {
 
         if (origin == null || destination == null) {
-            return false;
+            return Results.NULL_ARGUMENT;
         }
 
         if (!origin.isDirectory()) {
-            return false;
+            return Results.NOT_A_DIRECTORY;
         }
 
         if (destination.exists()) {
             if (!destination.isDirectory()) {
-                return false;
+                return Results.ALREADY_EXISTS;
             }
         } else {
             if (!destination.mkdirs()) {
-                return false;
+                return Results.IO_ERROR;
             }
         }
 
@@ -156,35 +176,62 @@ public class FileOperations {
                     File targetChild = new File(destination, child.getName());
 
                     if (!targetChild.exists() && !targetChild.mkdirs()) {
-                        return false;
+                        return Results.IO_ERROR;
                     }
 
-                    if (!copyDirectoryTree(child, targetChild)) {
-                        return false;
+                    Results result = copyDirectoryTree(child, targetChild);
+
+                    if (result != Results.OK) {
+                        return result;
                     }
 
                 } else if (child.isFile()) {
 
                     File targetChild = new File(destination, child.getName());
+                    Results result = copyFile(child, targetChild, false);
 
-                    if (!copyFile(child, targetChild, false)) {
-                        return false;
+                    if (result != Results.OK) {
+                        return result;
                     }
 
                 }
             }
         }
 
-        return true;
+        return Results.OK;
 
     }
 
-    public static boolean moveFile(File origin, File destination) {
-        return copyFile(origin, destination, false) && deleteRegularFile(origin);
+    public static Results moveFile(File origin, File destination) {
+        if (!(copyFile(origin, destination, false) == Results.OK)) {
+            return Results.COPY_ERROR;
+        }
+        if (!(deleteRegularFile(origin) == Results.OK)) {
+            return Results.DELETE_ERROR;
+        }
+        return Results.OK;
     }
 
-    public static boolean moveDirectoryTree(File origin, File destination) {
-        return copyDirectoryTree(origin, destination) && deleteDirectoryTree(origin);
+    public static Results moveDirectoryTree(File origin, File destination) {
+        if (!(copyDirectoryTree(origin, destination) == Results.OK)) {
+            return Results.COPY_ERROR;
+        }
+        if (!(deleteDirectoryTree(origin) == Results.OK)) {
+            return Results.DELETE_ERROR;
+        }
+        return Results.OK;
+    }
+
+    public enum Results {
+        OK,
+        NULL_ARGUMENT,
+        NOT_FOUND,
+        ALREADY_EXISTS,
+        IO_ERROR,
+        NOT_A_FILE,
+        NOT_A_DIRECTORY,
+        COPY_ERROR,
+        DELETE_ERROR
     }
 
 }
